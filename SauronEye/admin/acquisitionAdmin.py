31de235bc066978c054c2101e59cc7c561526de4
Lisa DRIE <lisa.drie@sogetrel.fr>
2024-04-12 11:14:33 +0200

    Projet SAURON

--- SauronEye/admin/acquisitionAdmin.py
+++ SauronEye/admin/acquisitionAdmin.py
@@ -3,7 +3,6 @@ from SauronEye.models.acquisition import Acquisition
 
 class AcquisitionAdmin(admin.ModelAdmin):
     list_filter=('Libelle',)
-    list_display=('Libelle')
     fieldsets = (
         (None, {
             'fields': (
